ddbd8e0891927d7529ebbf11909688ca168c2d37
Vorga <768699531@qq.com>
2020-11-12 20:41:54 +0800

    demo

---
+++ src/main/java/com/mxcsystem/demo/util/AzureUtil.java
@@ -0,0 +1,8 @@
+package com.mxcsystem.demo.util;
+
+
+public class AzureUtil {
+    private String INSTANCE = "124.160.107.91:10000";
+    private String COLLECTION = "2018级";
+    private String PROJECT = "40477-1-周圆小组";
+}
