01bb327c3a5265aeef669626ebc3ea700a4322da
Vorga <768699531@qq.com>
2020-12-03 22:06:13 +0800

    demo

--- src/main/java/com/mxcsystem/demo/controller/DemoController.java
+++ src/main/java/com/mxcsystem/demo/controller/DemoController.java
@@ -1,11 +1,23 @@
 package com.mxcsystem.demo.controller;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.mxcsystem.demo.util.CommonUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 @Controller
 public class DemoController {
+
+    @Resource
+    private DefaultKaptcha captchaProducer;
+
     @RequestMapping("test")
     @ResponseBody
     public String Test(){
@@ -16,5 +28,37 @@ public class DemoController {
     @ResponseBody
     public String Login(){
         return "test";
+    }
+
+    /**
+     * 登录验证码SessionKey
+     */
+    public static final String LOGIN_VALIDATE_CODE = "login_validate_code";
+    /**
+     * 登录验证码图片
+     */
+    @RequestMapping("/loginValidateCode")
+    public void loginValidateCode(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        CommonUtil.validateCode(request,response,captchaProducer,LOGIN_VALIDATE_CODE);
+    }
+
+    /**
+     * 检查验证码是否正确
+     */
+    @RequestMapping("/checkLoginValidateCode")
+    @ResponseBody
+    public HashMap checkLoginValidateCode(HttpServletRequest request,
+                                          @RequestParam("validateCode")String validateCode) {
+        String loginValidateCode = request.getSession().getAttribute(LOGIN_VALIDATE_CODE).toString();
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        if(loginValidateCode == null){
+            map.put("status",null);//验证码过期
+        }else if(loginValidateCode.equals(validateCode)){
+            map.put("status",true);//验证码正确
+        }else {
+            map.put("status",false);//验证码不正确
+        }
+        map.put("code",200);
+        return map;
     }
 }
