9f3f16190531a59b66747f1ba3b2da6bb9aaa529
Vorga <768699531@qq.com>
2020-12-04 14:45:16 +0800

    demo

--- src/main/java/com/mxcsystem/demo/controller/LoginController.java
+++ src/main/java/com/mxcsystem/demo/controller/LoginController.java
@@ -4,18 +4,16 @@ import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.mxcsystem.demo.service.LoginService;
 import com.mxcsystem.demo.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
-@Controller
+@RestController
 public class LoginController {
     @Autowired
     private LoginService loginService;
@@ -37,7 +35,6 @@ public class LoginController {
      * 检查验证码是否正确
      */
     @RequestMapping("/checkLoginValidateCode")
-    @ResponseBody
     public HashMap<String,Object> checkLoginValidateCode(HttpServletRequest request,
                                           @RequestParam("validateCode")String validateCode) {
         String loginValidateCode = request.getSession().getAttribute(LOGIN_VALIDATE_CODE).toString();
@@ -55,11 +52,10 @@ public class LoginController {
 
     @SuppressWarnings("unchecked")
     @RequestMapping("/checkLogin")
-    @ResponseBody
     public HashMap<String,Object> checkLogin(HttpServletRequest request,
-                              @RequestParam("validateCode")String validateCode,
-                              @RequestParam("username")String username,
-                              @RequestParam("password")String password){
+                                 @RequestParam("validateCode")String validateCode,
+                                 @RequestParam("username")String username,
+                                 @RequestParam("password")String password){
         HashMap<String,Object> map = new HashMap<>();
         if(request.getSession().getAttribute("identity") != null){
             map = (HashMap<String,Object>) request.getSession().getAttribute("identity");
@@ -75,11 +71,11 @@ public class LoginController {
         if(loginValidateCode.equals(validateCode)){
             map.put("validateStatus",true);//验证码正确
             if(loginService.checkLogin(username, password)){
-                map.put("loginStatus","true");
+                map.put("loginStatus",true);
                 //保存登陆凭证map
                 request.getSession().setAttribute("identity",map);
             }else{
-                map.put("loginStatus","false");
+                map.put("loginStatus",false);
             }
         }else {
             map.put("validateStatus",false);//验证码不正确
